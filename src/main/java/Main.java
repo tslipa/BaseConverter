26c22d598223514055f8fb233de14f285c81bd8c
tslipa <p.cegiela@outlook.com>
2020-10-17 19:14:37 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        if (args.length < 3)
+        {
+            System.out.println("Too few arguments. Three required.");
+            return;
+        }
+
+        int baseA, baseB, number;
+        try
+        {
+            baseA = Integer.parseInt(args[0]);
+            baseB = Integer.parseInt(args[1]);
+            number = Integer.parseInt(args[2]);
+
+            int minBase = 2;
+            int maxBase = 10;
+            if (baseA < minBase || baseB < minBase || baseA > maxBase || baseB > maxBase)
+            {
+                throw new Exception("One of the bases is not between 2 and 10.");
+            }
+
+            int minNumber = 0;
+            if (number < minNumber)
+            {
+                throw new Exception("The number should be a positive integer.");
+            }
+        }
+        catch (NumberFormatException ex)
+        {
+            System.out.println("Wrong arguments. Positive integers required.");
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
